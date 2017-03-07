7a87094ed710540fa077f1e9fbcd7f1db7a6bc24
S Fujimoto <unicoshun@gmail.com>
2017-03-07 22:22:03 +0900

    ~#31

--- py1/py_3.py
+++ py1/py_3.py
@@ -15,4 +15,24 @@ def my_function():
     print("I'm the function!")
 
 
-my_function()
+# my_function()
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print("In the decorator!")
+            if number == 56:
+                print("Not running the function!")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator!")
+        return function_that_runs_func
+    return my_decorator
+
+
+@decorator_with_arguments(57)
+def my_function_too(x, y):
+    print(x + y)
+
+my_function_too(57, 67)
