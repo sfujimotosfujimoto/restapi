4fc258472a6ff57ca1efde3ce34a64afda4817ba
S Fujimoto <unicoshun@gmail.com>
2017-11-27 10:18:42 +0900

    first before sqlalchemy

--- flsk/app.py
+++ flsk/app.py
@@ -1,12 +1,73 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': "My wonderful store",
+        'items': [
+            {
+                'name': "My Item",
+                'price': 15.99
+            }
+        ]
+    }
+];
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
+# POST - used to receive data
+# GET - used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    # Iterate over stores
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+    # if the store names matches, return it
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name>/item {name: price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 if __name__ == '__main__':
     app.run(port=3000)
---
+++ section6/code/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+app = Flask(__name__)
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False # it turns off Flask's version of this but SQLAlchemy's version is on
+app.secret_key = 'sfujimoto'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=3000)
